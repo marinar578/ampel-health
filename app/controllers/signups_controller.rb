e8f4e0e355a0605e2963f560f605dd6a159369ea
Marina Rozova <marinar578@gmail.com>
2016-10-04 21:10:22 -0400

    Add code to save signups

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,46 @@
+class SignupsController < ApplicationController
+    def index
+        @signups = Signup.all
+    end
+
+    def show
+        @signup = Signup.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+      @signup = Signup.new(signup_params)
+ 
+      @signup.save
+      flash.alert = 'Successfully signed up'
+      redirect_to "/"
+    end
+
+    def edit
+      @signup = Signup.find(params[:id])
+    end
+
+    def update
+      @signup = Signup.find(params["id"])
+   
+      if @signup.update(params["signup"].permit(:name, :email, :message))
+        redirect_to signup_path
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @signup = Signup.find(params[:id])
+      @signup.destroy
+     
+      redirect_to signups_path
+    end
+
+    private
+        def signup_params
+            params.require(:signup).permit(:name, :email, :message)
+        end
+end
