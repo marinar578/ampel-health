7c35b6a9740691894590556cd89e9e49df2a8dd1
Marina Rozova <marinar578@gmail.com>
2016-10-06 01:15:11 -0400

    Fix new users bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
-      redirect_to '/admin'
+      redirect_to '/users'
     else
       redirect_to '/users/new'
     end
